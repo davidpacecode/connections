77632d420fc68afa6310a57b3a1c54b01c9106fc
davidpacecode <davidpace@gmail.com>
2026-07-04 14:21:07 -0500

    working on stimulus controller

--- app/javascript/controllers/selection_controller.js
+++ app/javascript/controllers/selection_controller.js
@@ -2,6 +2,10 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="selection"
 export default class extends Controller {
-  connect() {
+  static targets = [ "output" ]
+
+  toggle_appearance() {
+    this.outputTarget.textContent =
+      `Hello, world!`
   }
 }
