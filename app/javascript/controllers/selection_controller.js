45b0cb34172517307855ce789a920cbaf45a6d81
davidpacecode <davidpace@gmail.com>
2026-07-05 17:41:06 -0500

    working on enabling and disabling buttons based on selections

--- app/javascript/controllers/selection_controller.js
+++ app/javascript/controllers/selection_controller.js
@@ -2,15 +2,21 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="selection"
 export default class extends Controller {
-  static targets = [ "output" ]
+  static targets = [ "output", "suffle_button", "deselect_button", "submit_button" ]
+  static values = { count: { type: Number, default: 0 } }
 
   toggle_appearance() {
     const card = event.currentTarget
 
     if (card.getAttribute("appearance") === "accent") {
       card.removeAttribute("appearance")
+      this.countValue--;
     } else {
       card.setAttribute("appearance", "accent")
+      this.countValue++;
+      if (this.countValue === 4) {
+        submit_buttonTarget.removeAttribute("disabled")
+      }
     }
 
     // this.outputTarget.textContent =
