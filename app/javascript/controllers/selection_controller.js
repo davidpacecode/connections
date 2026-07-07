90c7c656b101eb51d03cb9b1a8c35a2100e7f317
David Pace <david.pace@hey.com>
2026-07-06 20:59:24 -0500

    i have the deselect and submit buttons enabling and disabling correctly. Now need to implement shuffle, deselect, and submit!

--- app/javascript/controllers/selection_controller.js
+++ app/javascript/controllers/selection_controller.js
@@ -2,7 +2,7 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="selection"
 export default class extends Controller {
-  static targets = [ "output", "suffle_button", "deselect_button", "submit_button" ]
+  static targets = [ "output", "shuffle_button", "deselect_button", "submit_button" ]
   static values = { count: { type: Number, default: 0 } }
 
   toggle_appearance() {
@@ -12,11 +12,22 @@ export default class extends Controller {
       card.removeAttribute("appearance")
       this.countValue--;
     } else {
-      card.setAttribute("appearance", "accent")
-      this.countValue++;
-      if (this.countValue === 4) {
-        submit_buttonTarget.removeAttribute("disabled")
+      if (this.countValue !== 4) {
+        card.setAttribute("appearance", "accent")
+        this.countValue++;
       }
+    }
+
+    if (this.countValue === 4) {
+      this.submit_buttonTarget.removeAttribute("disabled")
+    } else {
+      this.submit_buttonTarget.setAttribute("disabled", true)
+    }
+
+    if (this.countValue !== 0) {
+      this.deselect_buttonTarget.removeAttribute("disabled")
+    } else {
+      this.deselect_buttonTarget.setAttribute("disabled", true)
     }
 
     // this.outputTarget.textContent =
